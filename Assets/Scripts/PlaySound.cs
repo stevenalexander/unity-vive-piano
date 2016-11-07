4a81c61768501c35975d3e58678311e49b7cd761
Steven Alexander <steven.william.alexander@googlemail.com>
2016-11-07 23:00:08 +0000

    initial commit

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour
+{
+    private float exitStopDelay = 0.1f;
+    void OnTriggerEnter(Collider col)
+    {
+        GetComponent<AudioSource>().Play();
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        StartCoroutine(StopAfterDelay());
+    }
+
+    IEnumerator StopAfterDelay()
+    {
+        yield return new WaitForSeconds(exitStopDelay);
+        GetComponent<AudioSource>().Stop();
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Unity Vive Piano
+
+Unity demo making a simple 3 octave piano keyboard.
+
+## Asset store
+
+- [SteamVR plugin](https://www.assetstore.unity3d.com/en/#!/content/32647)
+- [Choir Piano Sound Pack (7 Octaves)](https://www.assetstore.unity3d.com/en/#!/content/27633)
